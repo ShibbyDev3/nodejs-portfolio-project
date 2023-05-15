a1e2b2ad272cd1a915cc1f95bf89114200a13236
Eddy Gomez <contact@eddyg.dev>
2023-05-14 20:06:09 -0400

    deployment test

--- server.js
+++ server.js
@@ -3,6 +3,11 @@ const app = express();
 const PORT = process.env.PORT || 3030;
 
 // your code
+app.use((req, res) => {
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "text/html");
+  res.end("<html><body><h1>This is an Express Server</h1></body></html>");
+});
 
 app.listen(PORT, () => {
   console.log(`server started on port ${PORT}`);
